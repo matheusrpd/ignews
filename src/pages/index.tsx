9c1d308b57e58bf4eeb7c7f6f7bdf6eae05befb7
matheusrpd <matheusrpd@gmail.com>
2021-10-05 17:59:07 -0300

    Adicionando estilização global.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>ignews</h1>
-  )
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1>ignews</h1>
+    </>
+  );
 }
